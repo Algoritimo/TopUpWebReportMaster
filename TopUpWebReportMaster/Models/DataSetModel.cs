322f9f5b6bca7497480834e749a36ee50fd0f8e9
Edilson D Mucanze <42839422+ScripTech@users.noreply.github.com>
2021-07-28 10:50:24 +0200

    Return dataset values on view

---
+++ TopUpWebReportMaster/Models/DataSetModel.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace TopUpWebReportMaster.Models {
+    public class DataSet {
+        public string ID { get; set; }
+        public string DATETIME { get; set; }
+        public string CLIENTNUMBER { get; set; }
+        public string ENTITYNUMBER { get; set; }
+        public string DOCUMENTTYPEID { get; set; }
+        public string DOCUMENTYEAR { get; set; }
+        public string DOCUMENTNUMBER { get; set; }
+        public string PROVISIONNUMBER { get; set; }
+        public string TIMEOUT { get; set; }
+    }
+}
+
+// DatasetService
